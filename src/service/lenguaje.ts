eefa8b7f0609d130d3db040cf38f59795e60c93a
Drezxzx <andres250197@hotmail.com>
2024-03-31 00:10:41 +0100

    actualizacion de lenguajes

--- src/service/lenguaje.ts
+++ src/service/lenguaje.ts
@@ -21,142 +21,128 @@
         name: "Alemán",
         code: "de",
         emoji: "🇩🇪"
-      },
-      {
+    },
+    {
         name: "Italiano",
         code: "it",
         emoji: "🇮🇹"
-      },
-      {
+    },
+    {
         name: "Portugués",
         code: "pt",
         emoji: "🇵🇹"
-      },
-      {
+    },
+    {
         name: "Chino",
         code: "zh-CN",
         emoji: "🇨🇳"
-      },
-      {
-        name: "Taiwanes",
-        code: "zh-TW",
-        emoji: "🇹🇼"
-      },
-      {
+    },
+    {
         name: "Japonés",
         code: "ja",
         emoji: "🇯🇵"
-      },
-      {
+    },
+    {
         name: "Coreano",
         code: "ko",
         emoji: "🇰🇷"
-      },
-      {
-        name: "Árabe",
-        code: "ar",
+    },
+    {
+        name: "Árabe (Egipcio)",
+        code: "ar-EG",
+        emoji: "🇪🇬"
+    },
+    {
+        name: "Árabe (Marroquí)",
+        code: "ar-MA",
+        emoji: "🇲🇦"
+    },
+    {
+        name: "Árabe (Argelino)",
+        code: "ar-DZ",
+        emoji: "🇩🇿"
+    },
+    {
+        name: "Árabe (Saudí)",
+        code: "ar-SA",
         emoji: "🇸🇦"
-      },
-      {
-        name: "Ruso",
-        code: "ru",
-        emoji: "🇷🇺"
-      },
-      {
+    },
+    {
+        name: "Árabe (Emiratí)",
+        code: "ar-AE",
+        emoji: "🇦🇪"
+    },
+    {
         name: "Hindi",
         code: "hi",
         emoji: "🇮🇳"
-      },
-      {
+    },
+    {
         name: "Bengalí",
         code: "bn",
         emoji: "🇧🇩"
-      },
-      {
+    },
+    {
         name: "Panyabí",
         code: "pa",
-        emoji: "🇮🇳"
-      },
-      {
+        emoji: "🇵🇰"
+    },
+    {
         name: "Telugu",
         code: "te",
         emoji: "🇮🇳"
-      },
-      {
+    },
+    {
         name: "Maratí",
         code: "mr",
         emoji: "🇮🇳"
-      },
-      {
+    },
+    {
         name: "Tamil",
         code: "ta",
         emoji: "🇮🇳"
-      },
-      {
+    },
+    {
         name: "Urdu",
         code: "ur",
         emoji: "🇵🇰"
-      },
-      {
+    },
+    {
         name: "Turco",
         code: "tr",
         emoji: "🇹🇷"
-      },
-      {
+    },
+    {
         name: "Persa",
         code: "fa",
         emoji: "🇮🇷"
-      },
-      {
+    },
+    {
         name: "Indonesio",
         code: "id",
         emoji: "🇮🇩"
-      },
-      {
+    },
+    {
         name: "Malayo",
         code: "ms",
         emoji: "🇲🇾"
-      },
-      {
+    },
+    {
         name: "Vietnamita",
         code: "vi",
         emoji: "🇻🇳"
-      },
-      {
+    },
+    {
         name: "Tailandés",
         code: "th",
         emoji: "🇹🇭"
-      },
-      {
+    },
+    {
         name: "Filipino",
         code: "fil",
         emoji: "🇵🇭"
-      },
-      {
-        name: "Egipcio",
-        code: "ar-EG",
-        emoji: "🇪🇬"
-      },
-      {
-        name: "Marroquí",
-        code: "ar-MA",
-        emoji: "🇲🇦"
-      },
-      {
-        name: "Argelino",
-        code: "ar-DZ",
-        emoji: "🇩🇿"
-      },
-      {
-        name: "Saudí",
-        code: "ar-SA",
-        emoji: "🇸🇦"
-      },
-      {
-        name: "Emiratí",
-        code: "ar-AE",
-        emoji: "🇦🇪"
-      }
-  ]
+    }
+];
+
   
   export { LENGUAJES, OHTHERLANGUAJES}
